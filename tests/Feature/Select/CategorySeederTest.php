754ada10ad2f92a4ceb1850e3be9a37f4a202a02
Arief-Karditya <nolifegansaya@gmail.com>
2024-09-03 09:58:34 +0700

    Select

---
+++ tests/Feature/Select/CategorySeederTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature\Select;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategorySeederTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function testSelect(): void
+    {
+        for ($i = 1; $i <= 5; $i++) {
+            $category = new Category();
+            $category->id = "DUMMY-$i";
+            $category->name = "Category dummy $i";
+            $category->save();
+        }
+
+        // $categories = Category::query()->whereNull("description")->get();
+        $categories = Category::whereNull("description")->get();
+
+        self::assertEquals(5, $categories->count());
+        $categories->each(function ($category) {
+            self::assertNull($category->description);
+        });
+
+        // $categories = Category::query()->whereNull("description")->get();
+        $categories = Category::whereNull("description")->get();
+        self::assertEquals(5, $categories->count());
+
+        $categories->each(function ($category) {
+            $category->description = "Category dummy description updated";
+            $category->update();
+        });
+    }
+}
