a36dc4df4619bbc152d97b3238ac1c835517303a
Arief-Karditya <nolifegansaya@gmail.com>
2024-09-04 10:27:11 +0700

    Query Local Scope

--- tests/Feature/Voucher/VoucherTest.php
+++ tests/Feature/Voucher/VoucherTest.php
@@ -34,4 +34,19 @@ class VoucherTest extends TestCase
         $voucher = Voucher::withTrashed()->where('name', 'Sample Voucher')->first();
         self::assertNotNull($voucher);
     }
+
+    public function testLocalScope(): void
+    {
+        $voucher = new Voucher();
+
+        $voucher->name = "Sample Voucher";
+        $voucher->is_active = true;
+        $voucher->save();
+
+        $total = Voucher::query()->active()->count();
+        self::assertEquals(1, $total);
+
+        $total = Voucher::query()->nonActive()->count();
+        self::assertEquals(0, $total);
+    }
 }
