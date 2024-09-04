b59e980f9079fa5d1ffb5283196146f39687c0a2
Arief-Karditya <nolifegansaya@gmail.com>
2024-09-04 17:24:32 +0700

    Query Builder Relationship

--- tests/Feature/Customer/CustomerWithWalletTest.php
+++ tests/Feature/Customer/CustomerWithWalletTest.php
@@ -2,8 +2,12 @@
 
 namespace Tests\Feature\Customer;
 
+use App\Models\Category;
 use App\Models\Customer;
+use App\Models\Wallet;
+use Database\Seeders\CategorySeeder;
 use Database\Seeders\CustomerSeeder;
+use Database\Seeders\ProductSeeder;
 use Database\Seeders\WalletSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -29,5 +33,45 @@ class CustomerWithWalletTest extends TestCase
         Log::info(json_encode($customer));
 
         self::assertEquals(1000000, $wallet->amount);
+    }
+
+    public function testInsertRelationship()
+    {
+
+        $customer = new Customer();
+        $customer->id = "TERRY";
+        $customer->name = "Terry";
+        $customer->email = "terry@racist.nigger";
+        $customer->save();
+
+        self::assertNotNull($customer);
+        Log::info(json_encode($customer));
+
+        $wallet = new Wallet();
+        $wallet->amount = 1000000;
+
+        $customer->wallet()->save($wallet);
+        self::assertNotNull($wallet);
+        Log::info(json_encode($wallet));
+    }
+
+    public function testSearchProduct()
+    {
+        self::seed(CategorySeeder::class);
+        self::seed(ProductSeeder::class);
+
+        $category = Category::query()->find('FOOD');
+        self::assertNotNull($category);
+        Log::info(json_encode($category));
+
+        $outOfStockProducts = $category->products()->where('stock', '<=', 0)->get();
+
+        self::assertNotNull($outOfStockProducts);
+        self::assertCount(5, $outOfStockProducts);
+
+        $outOfStockProducts->each(function ($product) {
+            self::assertNotNull($product);
+            Log::info(json_encode($product));
+        });
     }
 }
